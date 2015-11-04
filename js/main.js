ebecefc998ec62e95c587adaab6cec33045b5acf
marinaghattas <marinaghattas393@yahoo.com>
2015-11-03 21:21:14 -0500

    added loops

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 function setup() {
 createCanvas(windowWidth, windowHeight);
-    var x = 40;
-    var y = 40;
-   var gridWidth = 900;
+    var x = -50;
+    var y = 0;
+   var winWidth = 1200;
     for(var i=0; i<100; i++){
         // console.log(i);
         drawPattern(x,y);
 
         //increment our x
-        x += 170;
+        x += 100;
         //chev=ck if x is past the drid width
-        if(x > gridWidth){
+        if(x > winWidth){
             //set X back to the beginning
-            x=40;
+            x=-50;
             //move y down
-            y += 300;
+            y += 200;
         } // end if
     } //end for loop
 
@@ -29,25 +29,25 @@ createCanvas(windowWidth, windowHeight);
         triangle(startX,startY,startX+50,startY+100,startX+100,startY);
         //1st small
         fill('green')
-        triangle(startX+20,startY+20,startX+50,startY+80,startX+80,startY+20);
+        triangle(startX+20,startY,startX+50,startY+60,startX+80,startY);
         //2nd large
         fill('AntiqueWhite')
-        triangle(startX+50,startY+100,startX+100,startY,startX+150,startY+100);
+triangle(startX+50,startY+100,startX+100,startY,startX+150,startY+100);
         //2nd small
         fill('red')
-        triangle(startX+70,startY+80,startX+100,startY+20,startX+130,startY+80);
+        triangle(startX+70,startY+100,startX+100,startY+40,startX+130,startY+100);
         //3rd large
         fill('BurlyWood')
         triangle(startX+50,startY+100,startX+100,startY+200,startX+150,startY+100);
         //3rd small
         fill('green')
-        triangle(startX+70,startY+120,startX+100,startY+180,startX+130,startY+120);
+        triangle(startX+70,startY+100,startX+100,startY+160,startX+130,startY+100);
         //4th large
         fill('AntiqueWhite')
-        triangle(startX,startY+200,startY+50,startX+100,startY+100,startY+200);
+        triangle(startX,startY+200,startX+50,startY+100,startX+100,startY+200);
         //4th small
         fill('red')
-        triangle(startX+20,startY+180,startX+50,startY+120,startX+80,startY+180);
+        triangle(startX+20,startY+200,startX+50,startY+140,startX+80,startY+200);
 
     }
 
